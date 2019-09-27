d6eaa9966bfabaa5f3b122dc84eb01aa51646de9
Igor B <isburenkov@gmail.com>
2019-09-27 12:37:53 +0300

    Hello world

---
+++ test.py
@@ -0,0 +1,14 @@
+import anvil.server
+import gkeepapi
+
+anvil.serever.connect("QJVRB4LEFOFXTTI4QK3DVVXR-R4IKANFI5NQKZ2RD")
+
+@anvil.server.callable
+def test_call():
+    keep = gkeepapi.Keep()
+    success = keep.login('isburenkov@gmail.com', 'EpZiL6xOeH')
+    
+    note = keep.createNote('Todo', 'Eat breakfast')
+    note.pinned = True
+    note.color = gkeepapi.node.ColorValue.Red
+    keep.sync()
